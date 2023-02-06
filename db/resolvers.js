480df73537c9de674069d486b93c7b1f5e01eff1
Adhafabe <adhafabe@gmail.com>
2023-02-06 13:30:06 -0600

    Autentificacion de usuarios y obtencion de usuario por ID

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,11 +1,26 @@
 const Usuario = require('../models/Usuario')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config({ path: 'variables.env' });
+
+
+
+const crearToken = (usuario, secreta, expiresIn) => {
+    console.log(usuario);
+    const { id, email, nombre, apellido } = usuario;
+
+    return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn })
+}
 
 
 //Resolvers
 const resolvers = {
     Query: {
-        ObtenerCurso: () => "Resultado"
+        obtenerUsuario: async (_, { token }) => {
+            const usuarioId = await jwt.verify(token, process.env.SECRETA)
+            
+            return usuarioId
+        }
     },
     Mutation: {
         nuevoUsuario: async (_, { input }) => {
@@ -17,7 +32,7 @@ const resolvers = {
             }
             //Hashear password
             const salt = await bcryptjs.genSalt(10);
-            input.password = await bcryptjs.hash(password,salt);
+            input.password = await bcryptjs.hash(password, salt);
             //Guardarlo en la base de datos
             try {
                 const usuario = new Usuario(input);
@@ -25,6 +40,23 @@ const resolvers = {
                 return usuario;
             } catch (error) {
                 console.log(error);
+            }
+        },
+        autenticarUsuario: async (_, { input }) => {
+            const { email, password } = input
+            //Si el usuario existe
+            const existeUsuario = await Usuario.findOne({ email });
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe');
+            }
+            //Revisar si el password es correcto
+            const passwordCorrecto = new bcryptjs.compare(password, existeUsuario.password);
+            if (!passwordCorrecto) {
+                throw new Error('El password es incorrecto');
+            }
+            //Crear token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
             }
         }
     }
